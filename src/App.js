cb1e2b09d8330a2435985bdded5059050e1292d8
Vishnu Vardhan Pattem <vishnuvardhanpattem@gmail.com>
2024-05-28 10:42:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import AdmissionModule from './components/Teacher/AdmissionModule';
+import Home from './components/Home';
+import Student from './components/Student/Student';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  >
+
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/teacher' element={<AdmissionModule />} />
+          <Route path='/student' element={<Student /> } />
+        </Routes>
+        
+
     </div>
   );
 }
