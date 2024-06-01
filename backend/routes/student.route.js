9e4ab7db8a96982b1ec096182052d721a9a6fff2
SAIKIRAN-67 <111266836+SAIKIRAN-67@users.noreply.github.com>
2024-06-01 11:25:34 +0530

    Add files via upload

---
+++ backend/routes/student.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { student_details_verification, studentregistration } from "../controllers/student.controller.js";
+
+const router=express.Router();
+
+router.post("/studentregistration",studentregistration);
+
+router.post("/studentdetailsverification",student_details_verification);
+
+export default router;
